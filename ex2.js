acbcffae5c5357e1cdeb1c10d9f0bcea5f1461de
OlgaGureeva <gureeva.olya.04@mail.ru>
2025-05-30 15:32:10 +0300

    Create ex2.js

---
+++ ex2.js
@@ -0,0 +1,17 @@
+function checkType(x) {
+    switch (typeof x) {
+      case 'number': 
+        console.log('x - число');
+        break;
+        case'string':
+        console.log('x - строка');
+        break;
+      case 'boolean':
+        console.log('x - логический тип');
+        break;
+      default:
+        console.log('Тип х не определен')
+    }
+    }
+    checkType(5)
+    checkType(погода)
