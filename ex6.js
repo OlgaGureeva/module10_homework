91e5c5dcd612e2fc29592f1ceaf364592d507cbe
OlgaGureeva <gureeva.olya.04@mail.ru>
2025-06-11 19:31:35 +0300

    Delete ex6.js

--- ex6.js
+++
@@ -1,6 +0,0 @@
-function areAllElementsEqual(arr) {
-    return new Set(arr).size <= 1;
-  }
-  
-  console.log(areAllElementsEqual([1, 1, 1, 1])); // true
-  console.log(areAllElementsEqual([1, 2, 1, 1])); // false
